115dfe3258e22311770e6c82907852bb46615125
tanggx <tanggx@apus-it.cn>
2026-01-27 14:37:37 +0800

    更新

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [uni()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   server: {
